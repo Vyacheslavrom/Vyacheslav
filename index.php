ede562df541afa3e963344970daa8335a814c2c9
slavikr <slavikr@mail.ru>
2020-04-12 22:02:41 +0300

    second lesson

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 /* PHP.net*/
-echo "good!!!Русский";
-echo "<br />";
-echo "<b>Helo Русский</b>";
-echo "\u{03b2}";
-echo '<br />Good <br /> <b>Фигня</b> \u{03b2}\;
+    $x = 214748836477777;
+    echo $x;
+    echo '<br/>';
+    echo PHP_INT_MAX;
+
+    $d_1 = 1.5;
+    $d_2 = 9.8e5;
+    $d_3 = 9.8e-5;
+    echo "<br/>d_1 = $d_1 d_2 = $d_2 d_3 = $d_3<br/>";
+
+    $s = 'My string';
+    echo $s;
+    echo '<br />';
+
+    $b_0 = false;
+    $b_1 = true;// boolian
+    echo "b_0 = $b_0 b_1 = $b_1 <br />";
+
+    $obj = null;
+    echo "obj = $obj";
+
+    /*Array, Object, Resource, Collable будут рассмотрены позже*/
 ?>
-<p> А это HTML текст
