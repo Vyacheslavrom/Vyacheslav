5d8ea70e505f904efe352236d1cc3458debc77aa
slavikr <slavikr@mail.ru>
2020-04-12 21:17:45 +0300

    The first comment

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "good!!!";
+/* PHP.net*/
+echo "good!!!Русский";
+echo "<br />";
+echo "<b>Helo Русский</b>";
+echo "\u{03b2}";
+echo '<br />Good <br /> <b>Фигня</b> \u{03b2}\;
 ?>
+<p> А это HTML текст
