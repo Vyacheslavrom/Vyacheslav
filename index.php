549165a5c68765b9c233f31163b6e714f1a12f47
slavikr <slavikr@mail.ru>
2020-04-13 13:13:49 +0300

    Matematic Lesson

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 /* PHP.net*/
-define ('COLOR', '#c00');
-echo COLOR;
-echo "<br />";
-echo defined ('COLOR');
-
-/*Встроенные константы*/
-echo '<br />';
-echo __FILE__;
-echo '<br />';
-echo PHP_VERSION;
-
+$x = 7;
+$y = 10;
+$z = $x + $y;
+echo "x = $x <br> y = $y <br /> Сумма z = $z";
+echo "<br /> Разность x-y =" .($x-$y);
+echo "<br /> Произведение x*y =".($x*$y);
+echo "<br /> Результат деления x/y $x/$y=".($x/$y);
+echo "<br />Остаток от деления x на y =".($x%$y);
+echo "<br />x в степини y=".($x**$y);
+echo "<br />квадратный корень из x =".($x**(1/2));
+$x++;$x++;
+echo "<br /> $x";
+$x--;$x--;
+echo "<br /> $x";
 ?>
-
-<P style="color: <?=COLOR?>">HELO PIPL!!!</p>
