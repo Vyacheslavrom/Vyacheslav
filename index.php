70311cc18febc826a3e9764c42eb6ef5aaded97d
slavikr <slavikr@mail.ru>
2020-04-14 16:43:10 +0300

    a lesson string

--- index.php
+++ index.php
@@ -1,34 +1,16 @@
 <?php
 /* PHP.net*/
-$a = 5;
-$b = 3;
- if ($a < 5) echo 'Истина';
- else echo "Лож <br />";
- if ($a < 5 || $b = 3) echo 'Истина';
- else echo 'Лож';
+$s_1 = 'My string';
+$s_2 = 'New string';
+echo $s_1. '-'.$s_2.'<br />';
+$s = $s_1.$s_2;
+echo $s;
 
- #if (isset($_GET['com'])) $com = $_GET['com'];
-#else $com = false;
+echo "<br />Символ с индексом 3 (позиция №4) встроке \"$s_1\"={$s_1[3]} n <br />";
 
-#$com =isset($_GET['com'])? $_GET['com'] : false;
-
-$com = $_GET['com']?? false;
-echo "<br /><br /> $com";
-
-if ($com == 'add') { $summa = $a + $b;
-  echo "<br /> $summa";
-}
-elseif ($com == 'mult'){ $umnog = $a * $b;
- echo "<br /> $umnog";
-}
-
-
+$res = `date`;
+echo $res;
+echo "<br />";
+echo iconv("UTF-8","cp866", $res);
 
 ?>
-
-<div>
-  <br />
-  <a href="?com=add">Сложить</a>
-  <a href="?com=mult">Перемножить</a>
-
-</div>
