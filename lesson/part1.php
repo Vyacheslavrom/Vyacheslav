840ad1fff7d0560307fbf39b3251f4021328800d
slavikr <slavikr@mail.ru>
2020-04-15 18:59:09 +0300

    lesson debugging functions

--- lesson/part1.php
+++ lesson/part1.php
@@ -1,32 +1,16 @@
 <?php
-echo 'Lesson Part1';
-echo "<br /><br />";
-print_r ($_POST);
-echo "<br /><br />";
-print_r ($_REQUEST);
-echo "<br /><br />";
-$summa = false;
-if (isset($_POST["myform"])){
-echo "форма отправлена";
-echo "<br /><br />";
-$x = $_POST["x"]?? false;
-$y = $_POST["y"]?? false;
-if ($x !== false && $y !== false && is_numeric($x) && is_numeric ($y)) $summa = $x + $y;
-else $summa = false;
-}
-#echo "Сумма равна: $summa";
-if ($summa !== false): echo "Сумма равна: $summa";
-endif;
-?>
+$arr = [1, 3, 5, 10];
+foreach ($arr as &$v) $v *= 2;
+unset($v);
+//foreach ($arr as $v) echo $v.'-';
+#echo '<pre>';
+print_r ($arr);
 
-<form name="myform" action="<?=$_SERVER["REQUEST_URI"]?>" method="post">
-     <div>
-         X: <input type="text" name="x" />
-     </div>
-     <div>
-         Y: <input type="text" name="y" />
-     </div>
-     <div>
-         <input type="submit" name="myform" value="Сумма" />
-     </div>
-</form>
+var_dump ($arr);
+
+var_export ($arr);
+
+$d = array ( 0 => 2, 1 => 6, 2 => 10, 3 => 20, );
+print_r ($d);
+
+?>
